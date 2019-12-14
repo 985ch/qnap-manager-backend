b7cac50a854e83b7142b43d6522f4e1d5c785434
985ch <115839257@qq.com>
2019-12-14 23:47:09 +0800

    修正一个错误

--- app/schedule/auto_resource.js
+++ app/schedule/auto_resource.js
@@ -6,13 +6,13 @@
 module.exports = () => {
   return {
     schedule: {
-      cron: '0 20 4 * * *',
+      cron: '0 30 3 * * *',
       type: 'worker',
       disable: false,
     },
     async task(ctx) {
       ctx.logger.info('start resources update');
-      await ctx.service.resource.folder.runAllArchives();
+      await ctx.service.resource.folder.checkAll();
       ctx.logger.info('complete resources update');
     },
   };
